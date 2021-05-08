d086cc7373e9e6520b903e3c6b18cbd8ba67c1d2
Johannes Pagel <Johannes_Pagel@hotmail.de>
2021-05-08 23:58:40 +0200

    add requirements and setup.py

---
+++ regression-models/model.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pandas as pd
+
+
+class LinearRegression:
+    """Class for a Regression Analysis
+
+    Attributes
+    ----------
+    X : DataFrame
+            The input feature matrix without threshold column
+    y : DataFrame
+            The dependent variable
+    """
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def fit(self):
+        """Class for a Regression Analysis
+
+            Parameters
+            ----------
+            X : DataFrame
+                    The input feature matrix without threshold column
+            y : DataFrame
+                    The dependent variable
+
+            Returns
+            ----------
+
+            """
+
+
+        pass
+
+    def predict(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+
+
+
+
+class PolynomialRegression:
